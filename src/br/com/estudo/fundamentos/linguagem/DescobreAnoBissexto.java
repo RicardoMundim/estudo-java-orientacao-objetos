48c546f154d83a1182f1edbb9e1b945e3a122b37
Ricardo <ritiny@gmail.com>
2021-01-26 23:04:56 -0300

    Entrada de dados e calculadora de ano bissexto

---
+++ src/br/com/estudo/fundamentos/linguagem/DescobreAnoBissexto.java
@@ -0,0 +1,23 @@
+package br.com.estudo.fundamentos.linguagem;
+
+import java.util.Scanner;
+
+public class DescobreAnoBissexto {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ano: ");
+		
+		// obtém entrada do tipo int
+		int ano = entrada.nextInt();
+
+		if (ano % 400 == 0) {
+			System.out.println("O ano \"" + ano + "\" é um ano bissexto");
+		} else if (ano % 4 == 0 && ano % 100 != 0) {
+			System.out.println("O ano \"" + ano + "\" é um ano bissexto");
+		} else {
+			System.out.println("O ano \"" + ano + "\" não é um ano bissexto");
+		}
+	}
+
+}
